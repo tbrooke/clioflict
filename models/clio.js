91bc44a62d40520704a756f3a9f92875b68fc234
tbrooke <tom.brooke@gmail.com>
2013-07-30 08:58:17 -0400

    Basic set up of models

---
+++ models/clio.js
@@ -0,0 +1,27 @@
+// Model for users
+
+// Inspired by: https://github.com/madhums/node-express-mongoose-demo
+
+
+/**
+ * Module dependencies.
+ */
+
+var mongoose = require('mongoose')
+  , Schema = mongoose.Schema
+  , crypto = require('crypto')
+  , _ = require('underscore')
+  , authTypes = ['github', 'twitter', 'facebook', 'google']
+
+/**
+ * User Schema
+ */
+
+var UserSchema = new Schema({
+  name: { type: String, default: '' },
+  email: { type: String, default: '' },
+  username: { type: String, default: '' },
+  hashed_password: { type: String, default: '' },
+  salt: { type: String, default: '' },
+  authToken: { type: String, default: '' },  //  I don't think we need this
+})
