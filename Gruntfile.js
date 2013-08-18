490502bb52f846de42a09cbed68d2e8e06a57471
Adam Ferguson <Adam.Brugh.Ferguson@gmail.com>
2013-08-18 18:04:34 -0400

    Set up bower and grunt and add jquery, bootstrap, and angular

---
+++ Gruntfile.js
@@ -0,0 +1,77 @@
+var path = require('path');
+
+module.exports = function(grunt) {
+  var angularPath = path.join(__dirname, 'bower_components/angular-1.1.6');
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    exec: {
+      'ng-deps': {
+        cwd: angularPath,
+        command: 'npm install'
+      },
+      'ng-build': {
+        cwd: angularPath,
+        command: 'grunt package'
+      }
+    },
+    clean: {
+      preBuild: ['public/css/app.css','public/js/app.js'],
+      postBuild: []
+    },
+    copy: {
+      bootstrapFonts: {
+        src: '',
+        dest: ''
+      }
+    },
+    concat: {
+      options: {
+        separator: '\n\n',
+        stripBanners: true,
+        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+          '<%= grunt.template.today("yyyy-mm-dd") %> */'
+      },
+      js: {
+        src: [
+          'bower_components/jquery/jquery.min.js',
+          'bower_components/bootstrap/dist/js/bootstrap.min.js',
+          'bower_components/angular-1.1.6/build/angular.js',
+          'bower_components/angular-1.1.6/build/angular-resource.js',
+          'bower_components/angular-1.1.6/build/angular-cookies.js'
+        ],
+        dest: 'public/js/app.js'
+      },
+      css: {
+        src: [
+          'bower_components/bootstrap/dist/css/bootstrap.min.css',
+          'public/stylesheets/style.css'
+        ],
+        dest: 'public/css/app.css'
+      }
+    },
+    watch: {
+      templates: {
+        files: [],
+        tasks: ['build']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-exec');
+
+  grunt.registerTask('build', 
+    [
+      'clean:preBuild',
+      'concat',
+      'clean:postBuild'
+    ]);
+
+  grunt.registerTask('default', ['watch']);
+  grunt.registerTask('buildAngular', ['exec', 'exec:ng-deps', 'exec:ng-build'])
+
+};
--- readme.md
+++ readme.md
@@ -1,11 +1,26 @@
 
 
-
-## [Clio] (http://www.goclio.com/?gclid=CMOuncr4z7gCFcdr7Aod1TwAgw) Conflict Checking System
-
-
+## [Clio](http://www.goclio.com/?gclid=CMOuncr4z7gCFcdr7Aod1TwAgw) Conflict Checking System
 
 
 A System to perform conflict checking across multiple instances of the Clio Law Practice Management system.
 
+## Development mode
 
+Make sure dependencies are installed:
+
+```bash
+npm install -g grunt-cli bower nodemon && npm install
+```
+
+Then, in one terminal run:
+
+```bash
+bower install && grunt build
+```
+
+In another terminal run:
+
+```bash
+nodemon app.js
+```
