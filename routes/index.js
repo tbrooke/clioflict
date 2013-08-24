78cffaf05717cbe5ff1d237070471995211f5b05
tbrooke <tom.brooke@gmail.com>
2013-08-24 15:46:01 -0400

    Got Tabs Working

--- routes/index.js
+++ routes/index.js
@@ -16,8 +16,8 @@ module.exports = function(app) {
 	app.get('/admin', ensureLoggedIn('/login'), admin);
 	app.get('/login', auth.loginForm);
 	app.post('/login', auth.login);
-  app.get('/signup',  ensureLoggedIn('/login'), auth.signupForm);
-  app.post('/signup', ensureLoggedIn('/login'), auth.signup);
+  app.get('/signup',  ensureLoggedIn('/signup'), auth.signupForm);
+  app.post('/signup', ensureLoggedIn('/signup'), auth.signup);
 	app.get('/logout', auth.logout);
 	app.get('/callback', clio.callback);
 	app.get('/clioAuth', ensureLoggedIn('/login'), clio.clioAuth);
