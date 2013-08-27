28bd4ab4f0cd2469ac2c84bf3dd28a9e832fd5b3
tbrooke <tom.brooke@gmail.com>
2013-08-26 21:33:53 -0400

    fixed reg to req

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,14 @@
 var ClioAccount = require('../models').ClioAccount;
-var User = require('../models').User;
+var User = require('../models').Users;
 
 exports.admin = function(req, res){
   accountIds = req.user.clioAccountIds;
+  userIds = req.user.userIds;
   ClioAccount.in('_id', accountIds).exec(function(err,accounts) {
     res.render('admin', { title: 'Admin', accounts: accounts, req: req });
   });
-
-// exports.user = function(req,res) {
-// 	userIds = reg.userIds;
-//     User.in('_id',userIds).exec(function(err,users){
-//   	res.render('admin', { title: 'Admin', users: users, req: req });
-//   });
+  User.in('_id',userIds).exec(function(err,users){
+  	res.render('admin', { users: users, req: req });
+  });
 };
 
