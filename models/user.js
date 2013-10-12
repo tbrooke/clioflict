33480b6ad7aeb52dab8dfd87e12bdcaf999c28ab
tbrooke <tom.brooke@gmail.com>
2013-10-12 16:19:25 -0400

    initial integration with passport

--- models/user.js
+++ models/user.js
@@ -8,7 +8,7 @@ var userSchema = mongoose.Schema({
   email: { type: String, required: true, index: {unique: true}},
   hashedPassword: String,
   salt: String,
-  loginAttempts: { type: number, required: true, default: 0},
+  loginAttempts: { type: Number, required: true, default: 0},
   lockUntil: {type: Number},
   authenticationToken: { type: String, index: {unique: true} },
   resetToken: String,
@@ -28,16 +28,42 @@ userSchema.virtual('password').set(function(password){
     this.hashedPassword = this.encryptPassword(password);
   }).get(function(){ return this._password; });
 
+    
+userSchema.virtual('authenticate').get(function(email,hashedPassword,err) {
 
-userSchema.virtual('getAuthenticated').set(function(passwordConfirmation){
-    this._passwordConfirmation = passwordConfirmation;
-  }).get(function(){ return this._passwordConfirmation; });
+        if (err) throw err;
+
+        // login was successful if we have a user
+        if (user) {
+            // handle login success
+            console.log('login success');
+            return;
+        }
+
+        // otherwise we can determine why we failed
+        var reasons = User.failedLogin;
+        switch (reason) {
+            case reasons.NOT_FOUND:
+            case reasons.PASSWORD_INCORRECT:
+                // note: these cases are usually treated the same - don't tell
+                // the user *why* the login failed, only that it did
+                break;
+            case reasons.MAX_ATTEMPTS:
+                 return done(null, false, { message: 'To Many Attempts Try again later ' }); 
+                break; }
+        }
+    });
+
+
+// .set(function(passwordConfirmation){
+//     this._passwordConfirmation = passwordConfirmation;
+//   }).get(function(){ return this._passwordConfirmation; });
 
 userSchema.methods.authenticate = function(plainText) {
   return this.encryptPassword(plainText) === this.hashedPassword;
 };
 
-userSchema.statistics.failedLogin = {
+userSchema.statics.failedLogin = {
     NOT_FOUND: 0,
     PASSWORD_INCORRECT: 1,
     MAX_ATTEMPTS: 2
@@ -71,7 +97,35 @@ userSchema.statics.getAuthenticated = function(email, hashedPassword, cb) {
                 if (err) return cb(err);
                 return cb(null, null, reasons.MAX_ATTEMPTS);
             });
-        }
+          }
+         // test for a matching password
+        user.comparePassword(password, function(err, isMatch) {
+            if (err) return cb(err);
+
+            // check if the password was a match
+            if (isMatch) {
+                // if there's no lock or failed attempts, just return the user
+                if (!user.loginAttempts && !user.lockUntil) return cb(null, user);
+                // reset attempts and lock info
+                var updates = {
+                    $set: { loginAttempts: 0 },
+                    $unset: { lockUntil: 1 }
+                };
+                return user.update(updates, function(err) {
+                    if (err) return cb(err);
+                    return cb(null, user);
+                });
+
+            // password is incorrect, so increment login attempts before responding
+            user.incLoginAttempts(function(err) {
+                if (err) return cb(err);
+                return cb(null, null, reasons.PASSWORD_INCORRECT);
+            });
+        };
+    });
+  });
+};
+
 
 userSchema.methods.makeSalt = function() {
   return Math.round((new Date().valueOf() * Math.random())) + '';
